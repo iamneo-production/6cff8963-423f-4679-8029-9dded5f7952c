5ed900cb92d771c2a3c3995a6d1fe2f405be8171
RambathriVishal <vishalrambathri02@gmail.com>
2023-06-29 07:24:17 +0000

    first commit

---
+++ reactapp/src/users/EditUser.js
@@ -0,0 +1,135 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
+export default function EditUser() {
+  let navigate = useNavigate();
+
+  const { id } = useParams();
+ const [user, setUser] = useState({
+    name: "",
+    email: "",
+    taskname:"",
+    description:"",
+    duedate:"",
+    status:"",
+  });
+
+ const { name, email,taskname,description,duedate,status } = user;
+
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.put(`http://localhost:8032/user/${id}`, user);
+    navigate("/");
+  };
+
+  const loadUser = async () => {
+    const result = await axios.get(`http://localhost:8032/user/${id}`);
+    setUser(result.data);
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">Edit User</h2>
+
+          <form onSubmit={(e) => onSubmit(e)}>
+           <div className="mb-3">
+              <label htmlFor="Name" className="form-label">
+                Name
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter name"
+                name="name"
+                value={name}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Email" className="form-label">
+                E-mail
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter  e-mail address"
+                name="email"
+                value={email}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+                <div className="mb-3">
+              <label htmlFor="Taskname" className="form-label">
+               TaskName
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter Taskname"
+                name="taskname"
+                value={taskname}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Description" className="form-label">
+              Description
+              </label>
+              <textarea
+                type={"text"}
+                className="form-control"
+                placeholder="Enter Description"
+                name="description"
+                value={description}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="DueDate" className="form-label">
+              DueDate
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter  Duedate"
+                name="duedate"
+                value={duedate}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Status" className="form-label">
+               Status
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter Status"
+                name="status"
+                value={status}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <button type="submit" className="btn btn-outline-primary">
+              Submit
+            </button>
+            <Link className="btn btn-outline-danger mx-2" to="/">
+              Cancel
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
