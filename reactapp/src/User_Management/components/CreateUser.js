4234a3ce5b030713bc7750aa50bc2dc5937eb9f3
haripriyadec <sec19ei030@sairamtap.edu.in>
2023-06-30 15:25:14 +0000

    user mgmt

--- reactapp/src/User_Management/components/CreateUser.js
+++ reactapp/src/User_Management/components/CreateUser.js
@@ -1,119 +1,128 @@
-import React, { useState } from "react";
-import HeaderListUser from "./HeaderCreateUser";
-import { validateEmployeeId, validateEmail, validateName, validatePassword } from "./ValidationUtils";
-import "../components_styles/create_user.css";
-
+import React, { useState } from 'react';
+import HeaderListUser from './HeaderCreateUser';
+import { validateEmail, validateName, validatePassword } from './ValidationUtils';
+import '../components_styles/create_user.css';
 
 export default function CreateUser() {
   const [formData, setFormData] = useState({
-    name: "",
-    surname: "",
-    emprole: "",
-    emailid: "",
-    passw: "",
+    name: '',
+    surname: '',
+    emprole: '',
+    emailid: '',
+    passw: '',
   });
 
   const [errors, setErrors] = useState({
-    name: "",
-    surname: "",
-    emprole: "",
-    emailid: "",
-    passw: "",
+    name: '',
+    surname: '',
+    emprole: '',
+    emailid: '',
+    passw: '',
   });
 
   function restartForm() {
     setFormData({
-      name: "",
-      surname: "",
-      emprole: "",
-      emailid: "",
-      passw: "",
+      name: '',
+      surname: '',
+      emprole: '',
+      emailid: '',
+      passw: '',
     });
 
     setErrors({
-      name: "",
-      surname: "",
-      emprole: "",
-      emailid: "",
-      passw: "",
+      name: '',
+      surname: '',
+      emprole: '',
+      emailid: '',
+      passw: '',
     });
   }
 
   function handleChange(e) {
     const { name, value } = e.target;
     const trimmedValue = value.trim();
-  
-    let errorMessage = "";
+
+    let errorMessage = '';
     switch (name) {
-      case "emprole":
-        if (!["admin", "employee"].includes(trimmedValue)) {
+      case 'emprole':
+        if (!['admin', 'employee'].includes(trimmedValue)) {
           errorMessage = "Invalid employee role. Please choose either 'admin' or 'employee'.";
         }
         break;
-      case "emailid":
+      case 'emailid':
         errorMessage = validateEmail(trimmedValue);
         break;
-      case "name":
-      case "surname":
+      case 'name':
+      case 'surname':
         errorMessage = validateName(trimmedValue);
         break;
-      case "passw":
+      case 'passw':
         errorMessage = validatePassword(trimmedValue);
         break;
       default:
         break;
     }
-  
+
     // Check for empty values and update the error message
-    if (trimmedValue === "") {
-      errorMessage = "This field is required.";
+    if (trimmedValue === '') {
+      errorMessage = 'This field is required.';
     }
-  
+
     setErrors((prevErrors) => ({
       ...prevErrors,
       [name]: errorMessage,
     }));
-  
+
     setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: trimmedValue,
     }));
   }
- 
 
-  function addUser(e) {
+  async function addUser(e) {
     e.preventDefault();
-  
+
     // Check for validation errors
-    if (!formData.name || !formData.surname || !formData.emprole || !formData.emailid || !formData.passw || errors.name ||
-      errors.surname || errors.emprole ||  errors.emailid ||   errors.passw) {
+    if (
+      !formData.name ||
+      !formData.surname ||
+      !formData.emprole ||
+      !formData.emailid ||
+      !formData.passw ||
+      errors.name ||
+      errors.surname ||
+      errors.emprole ||
+      errors.emailid ||
+      errors.passw
+    ) {
       // Handle validation errors
       setErrors((prevErrors) => ({
         ...prevErrors,
-        name: !formData.name ? "This field is required." : prevErrors.name,
-        surname: !formData.surname ? "This field is required." : prevErrors.surname,
-        emprole: !formData.empid ? "This field is required." : prevErrors.emprole,
-        emailid: !formData.emailid ? "This field is required." : prevErrors.emailid,
-        passw: !formData.passw ? "This field is required." : prevErrors.passw,
+        name: !formData.name ? 'This field is required.' : prevErrors.name,
+        surname: !formData.surname ? 'This field is required.' : prevErrors.surname,
+        emprole: !formData.emprole ? 'This field is required.' : prevErrors.emprole,
+        emailid: !formData.emailid ? 'This field is required.' : prevErrors.emailid,
+        passw: !formData.passw ? 'This field is required.' : prevErrors.passw,
       }));
       return;
     }
-  
+
     // Proceed with user creation
-    const request = new Request("http://localhost:8080/api/users", {
-      method: "POST",
+    const request = new Request('http://localhost:8080/api/users', {
+      method: 'POST',
       body: JSON.stringify(formData),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     });
-  
+
     fetch(request)
       .then((res) => {
         if (res.ok) {
+          sendEmail(formData.emailid); // Send email to the created user
           return res.json(); // Return the parsed JSON data
         } else {
-          throw new Error("Failed to create user"); // Throw an error if the response is not successful
+          throw new Error('Failed to create user'); // Throw an error if the response is not successful
         }
       })
       .then(() => {
@@ -123,7 +132,26 @@ export default function CreateUser() {
         console.error(error);
       });
   }
-  
+
+  async function sendEmail(email) {
+    const request = new Request('http://localhost:8080/api/send-email', {
+      method: 'POST',
+      body: JSON.stringify({ email }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    fetch(request)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Failed to send email');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
   return (
     <>
@@ -138,7 +166,7 @@ export default function CreateUser() {
             name="name"
             value={formData.name}
             required
-            className={errors.name ? "error-input" : ""}
+            className={errors.name ? 'error-input' : ''}
           />
           {errors.name && <span className="error-message">{errors.name}</span>}
 
@@ -149,25 +177,22 @@ export default function CreateUser() {
             name="surname"
             value={formData.surname}
             required
-            className={errors.surname ? "error-input" : ""}
+            className={errors.surname ? 'error-input' : ''}
           />
-          {errors.surname && (
-            <span className="error-message">{errors.surname}</span>
-          )}
-         <label>Employee Role</label>
+          {errors.surname && <span className="error-message">{errors.surname}</span>}
+          <label>Employee Role</label>
           <select
             onChange={handleChange}
             name="emprole"
             value={formData.emprole}
             required
-            className={errors.emprole ? "error-input" : ""}
+            className={errors.emprole ? 'error-input' : ''}
           >
             <option value="">Select Role</option>
             <option value="admin">Admin</option>
             <option value="employee">Employee</option>
           </select>
           {errors.emprole && <span className="error-message">{errors.emprole}</span>}
-
 
           <label>Email ID</label>
           <input
@@ -176,11 +201,9 @@ export default function CreateUser() {
             name="emailid"
             value={formData.emailid}
             required
-            className={errors.emailid ? "error-input" : ""}
+            className={errors.emailid ? 'error-input' : ''}
           />
-          {errors.emailid && (
-            <span className="error-message">{errors.emailid}</span>
-          )}
+          {errors.emailid && <span className="error-message">{errors.emailid}</span>}
 
           <label>Password</label>
           <input
@@ -189,11 +212,9 @@ export default function CreateUser() {
             name="passw"
             value={formData.passw}
             required
-            className={errors.passw ? "error-input" : ""}
+            className={errors.passw ? 'error-input' : ''}
           />
           {errors.passw && <span className="error-message">{errors.passw}</span>}
-
-
 
           <button onClick={addUser} className="createBtn">
             Add User
