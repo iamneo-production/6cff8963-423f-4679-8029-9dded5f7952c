5af0f80816c77979e965ae531da1b410c7617cc4
prakashbarui2001 <baruiprakash2020@gmail.com>
2023-06-30 10:07:26 +0000

    login comonent added

---
+++ reactapp/src/components/login_component.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+export default function Login() {
+  const [setEmail] = useState("");
+  const [setPassword] = useState("");
+
+  return (
+    <div className="auth-wrapper">
+      <div className="auth-inner">
+        <form>
+          <h3>LOGIN</h3>
+
+          <div className="mb-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-3">
+            <div className="custom-control custom-checkbox">
+              <input
+                type="checkbox"
+                className="custom-control-input"
+                id="customCheck1"
+              />
+              <label className="custom-control-label" htmlFor="customCheck1">
+                Remember me
+              </label>
+            </div>
+          </div>
+
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="forgot-password text-right">
+            <a href="/sign-up">Register</a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+}
