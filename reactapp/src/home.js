8fd0c5f7507472fb279be9626d081a99092cae7b
MohanSaiPK <sai17niki@gmail.com>
2023-06-30 05:43:43 +0000

    Added view task in home.js

--- reactapp/src/home.js
+++ reactapp/src/home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
 function Home() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
-      <Link to="/taskform">
+      <Link to="/task-form">
         <button>Create Task</button>
+      </Link>
+      
+      <Link to="/task-list">
+        <button>View all Task</button>
       </Link>
     </div>
   );
